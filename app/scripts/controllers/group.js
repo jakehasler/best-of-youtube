d0fba6d14c441e8faff7715abcddf39087c4e429
Jake Hasler <jakehasler@gmail.com>
2016-01-24 01:42:43 -0700

    Enhanced error checking. Only allows YouTube links.

--- app/scripts/controllers/group.js
+++ app/scripts/controllers/group.js
@@ -8,7 +8,7 @@
  * Controller of the bestOfYoutubeApp
  */
 angular.module('bestOfYoutubeApp')
-  .controller('GroupIdCtrl', function ($scope, $http, $firebaseObject, $firebaseArray, $mdDialog, $window) {
+  .controller('GroupIdCtrl', function ($scope, $http, $firebaseObject, $firebaseArray, $mdDialog, $window, $routeParams) {
 
   	var ref = new Firebase("https://the-best-of-youtube.firebaseio.com/urls");
     var ref2 = new Firebase("https://the-best-of-youtube.firebaseio.com/data");
@@ -18,6 +18,8 @@ angular.module('bestOfYoutubeApp')
     syncObject.$bindTo($scope, "data");
 
     $scope.urls = $firebaseArray(ref);
+
+    $scope.groupNumber = $routeParams.id;
 
     if(!parseInt(sessionStorage.getItem("upvotes"))) {
     	$window.sessionStorage.setItem("upvotes", 0);
@@ -74,7 +76,7 @@ angular.module('bestOfYoutubeApp')
 
 	 }
 
-	$scope.getYoutube = function(youtubeUrl) {
+	$scope.getYoutube = function(youtubeUrl, ev) {
 
 		var getParameterByName = function(name, youtubeUrl) {
 		 	var match = RegExp('[?&]' + name + '=([^&]*)').exec(youtubeUrl);
@@ -89,36 +91,56 @@ angular.module('bestOfYoutubeApp')
 			var test = RegExp('.be/([^]*)').exec(youtubeUrl);
 			test = decodeURIComponent(test[1].replace(/\+/g, ' '));
 			id = test;
+			$scope.runYouTubeData(id);
 		}
-		else {
+		else if(/youtube/.test(youtubeUrl)) {
 			console.log('Desktop URL');
 			id = getParameterByName("v", youtubeUrl);
+			$scope.runYouTubeData(youtubeUrl, id);
+		}
+		else {
+			$mdDialog.show(
+		      $mdDialog.alert()
+		        .parent(angular.element(document.querySelector('#popupContainer')))
+		        .clickOutsideToClose(true)
+		        .title('Sorry!')
+		        .textContent('Please Enter a valid YouTube URL')
+		        .ariaLabel('Alert Dialog Demo')
+		        .ok('Okay!')
+		        .targetEvent(ev)
+		    );
+		    $scope.newUrlText = "";
 		}
 
-		console.log("Inside getYoutube()")
+	}
 
-		var url = "https://www.googleapis.com/youtube/v3/videos?id=";
-		var videoID = id;
-		var apiKey = "AIzaSyDEOt5nyqmEtKbJxDXeb83IFkOnPPvqtYk";
-		var params = "&part=snippet,contentDetails,statistics,status";
+	$scope.runYouTubeData = function(youtubeUrl, id) {
+	console.log("Inside getYoutube()")
 
-		url = url + videoID + "&key=" + apiKey + params;
+	var url = "https://www.googleapis.com/youtube/v3/videos?id=";
+	var videoID = id;
+	var apiKey = "AIzaSyDEOt5nyqmEtKbJxDXeb83IFkOnPPvqtYk";
+	var params = "&part=snippet,contentDetails,statistics,status";
 
-		$http.get(url).success(function(res) {
-			console.log(res);
-			var youtube = {
-				title: res.items[0].snippet.title,
-				imgUrl: res.items[0].snippet.thumbnails.medium.url,
-				viewCount: res.items[0].statistics.viewCount,
-				likeCount: res.items[0].statistics.likeCount,
-				url: youtubeUrl,
-				upvotes: 0
-			};
+	url = url + videoID + "&key=" + apiKey + params;
 
-			$scope.urls.$add(youtube);
-			$scope.newUrlText = "";
+	$http.get(url).success(function(res) {
+		console.log(res);
+		var youtube = {
+			title: res.items[0].snippet.title,
+			imgUrl: res.items[0].snippet.thumbnails.medium.url,
+			viewCount: res.items[0].statistics.viewCount,
+			likeCount: res.items[0].statistics.likeCount,
+			url: youtubeUrl,
+			upvotes: 0
+		};
 
-		});
+		$scope.urls.$add(youtube);
+		$scope.newUrlText = "";
+
+	});
+
+		
 	}
 
 
